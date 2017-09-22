e9d074f35e7f2c1763f46e8143b5e601cae319ba
McAuliffe, Matthew <mcauliffem@gatech.edu>
2017-09-22 13:52:54 -0400

    This is the Project, it contains a login and homescreen, along with the buttons to control them.

---
+++ PracticeRatApp/app/src/main/java/com/example/android/practiceratapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.practiceratapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button toLogin = (Button) findViewById(R.id.goToLoginButton);
+        toLogin.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+    }
+}
